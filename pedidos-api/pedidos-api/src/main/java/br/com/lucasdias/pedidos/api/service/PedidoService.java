9abc6dd0255f6684d31f2b6cfaa044a67e2b82d1
Luc4sf4 <superlucas2005@gmail.com>
2025-03-10 10:58:08 -0300

    Criando Service de Pedido

---
+++ pedidos-api/pedidos-api/src/main/java/br/com/lucasdias/pedidos/api/service/PedidoService.java
@@ -0,0 +1,29 @@
+package br.com.lucasdias.pedidos.api.service;
+
+import br.com.lucasdias.pedidos.api.entity.Pedido;
+import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+    private final Logger logger = LoggerFactory.getLogger(PedidoService.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public Pedido enfileirarPedido(Pedido pedido){
+        rabbitTemplate.convertAndSend(exchangeName, "", pedido);
+        logger.info("pedido enfileirado", pedido.toString());
+        return pedido;
+    }
+
+}
