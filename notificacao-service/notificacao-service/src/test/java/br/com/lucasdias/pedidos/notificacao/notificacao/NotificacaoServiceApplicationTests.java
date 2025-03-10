2f45e3736616b52e1ef6519393e35bac1a81dee4
Luc4sf4 <superlucas2005@gmail.com>
2025-03-10 11:12:30 -0300

    Criando e adicionando novas dependencias ao projeto de microsservico de notificacao via email

---
+++ notificacao-service/notificacao-service/src/test/java/br/com/lucasdias/pedidos/notificacao/notificacao/NotificacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasdias.pedidos.notificacao.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
