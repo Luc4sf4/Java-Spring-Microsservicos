3f63f874d0d9da1a3b86d506813b33c66adc36b5
Luc4sf4 <superlucas2005@gmail.com>
2025-03-10 11:02:40 -0300

    Enviando os pedidos para o RabbitMQ

--- pedidos-api/pedidos-api/src/main/java/br/com/lucasdias/pedidos/api/controller/PedidoController.java
+++ pedidos-api/pedidos-api/src/main/java/br/com/lucasdias/pedidos/api/controller/PedidoController.java
@@ -1,6 +1,7 @@
 package br.com.lucasdias.pedidos.api.controller;
 
 import br.com.lucasdias.pedidos.api.entity.Pedido;
+import br.com.lucasdias.pedidos.api.service.PedidoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,15 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/pedidos")
 public class PedidoController {
 
-    private  final Logger logger = LoggerFactory.getLogger(PedidoController.class);
+    private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
+
+    @Autowired
+    private PedidoService pedidoService;
 
     @Operation(summary = "Cria novo pedido", description = "Contem as operacoes para cirar um novo pedido",
-    responses = @ApiResponse(responseCode = "201", description = "recurso cirado com sucesso"
-    , content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pedido.class)))
+            responses = @ApiResponse(responseCode = "201", description = "recurso cirado com sucesso"
+                    , content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pedido.class)))
     )
     @PostMapping
-    public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido){
+    public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido) {
         logger.info("Pedido recebido: {}", pedido.toString());
+        pedido = pedidoService.enfileirarPedido(pedido);
         return ResponseEntity.status(HttpStatus.CREATED).body(pedido);
     }
 }
--- pedidos-api/pedidos-api/src/main/java/br/com/lucasdias/pedidos/api/service/PedidoService.java
+++ pedidos-api/pedidos-api/src/main/java/br/com/lucasdias/pedidos/api/service/PedidoService.java
@@ -26,4 +26,6 @@ public class PedidoService {
         return pedido;
     }
 
+
+
 }
