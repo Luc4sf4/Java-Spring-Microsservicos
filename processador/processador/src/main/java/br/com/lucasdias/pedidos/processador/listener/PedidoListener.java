9e5bc20b15cc7b0880cd66b9d88d74c7fceb2535
Luc4sf4 <superlucas2005@gmail.com>
2025-03-11 14:51:17 -0300

    enviando pedido, recebendo emai e salvando no banco de dados

--- processador/processador/src/main/java/br/com/lucasdias/pedidos/processador/listener/PedidoListener.java
+++ processador/processador/src/main/java/br/com/lucasdias/pedidos/processador/listener/PedidoListener.java
@@ -2,9 +2,11 @@ package br.com.lucasdias.pedidos.processador.listener;
 
 import br.com.lucasdias.pedidos.processador.entity.Pedido;
 import br.com.lucasdias.pedidos.processador.entity.enums.Status;
+import br.com.lucasdias.pedidos.processador.service.PedidoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,11 +14,15 @@ public class PedidoListener {
 
     private final Logger logger = LoggerFactory.getLogger(PedidoListener.class );
 
+    @Autowired
+    private PedidoService pedidoService;
+
     @RabbitListener(queues = "pedidos.v1.pedido-criado.gerar-processamento")
     public void salvarPedido(Pedido pedido){
 
         pedido.setStatus(Status.PROCESSADO);
-        logger.info("Pedido processado: {}", pedido.toString());
+        pedidoService.save(pedido);
+
     }
 
 }
